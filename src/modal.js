606a2f3f95120092b1ffba845973c6417d0f7a45
Keago99 <keatabde@gmail.com>
2024-01-12 20:27:27 +0200

    added colors for dropdown, classes and working on changing dropdown with an event

---
+++ src/modal.js
@@ -0,0 +1,7 @@
+// This is where all the javascript relating to the modal will go.
+
+function changePriority(){
+    const priorityButton = document.getElementById("Priority");
+    
+    
+}
--- src/projects.js
+++ src/projects.js
@@ -21,6 +21,7 @@ function loadProjects(){
     console.log(basicTodo);
 }
 
+// A function to show the hidden project add form with its accept and cancel buttons
 function showProjAdd(){
     const projectAdd = document.getElementById("hiddenProjAdd");
     const addProjButton = document.getElementById("addProjButton");
@@ -30,13 +31,38 @@ function showProjAdd(){
 
 }
 
+// a function to clear the project form and hide its contents, including the two buttons, this should
+// also reshow the "add project" button.
+function cancelProjAdd(){
+    const hiddenForm = document.getElementById("hiddenProjAdd");
+    const addProjButton = document.getElementById("addProjButton");
+    const input = document.getElementById("inputText");
+
+    input.value = "";
+
+    hiddenForm.style.display = "none";
+    addProjButton.style.display = "block";
+
+}
+
+
+// Adding all eventlisteners that have to do with the project class.
 function projListeners(){
     const addProjButton = document.getElementById("addProjButton");
+    const cancelButton = document.getElementById("cancelButton");
 
-    console.log("this works!");
+
     addProjButton.addEventListener("click", () => {
         showProjAdd();
     })
+
+    cancelButton.addEventListener("click", () => {
+        cancelProjAdd();
+    })
+
+
+
+    
 }
 
 export {projListeners, loadProjects, starterProject, AllProjects};
