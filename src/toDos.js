33b706bbe0430f846ecb85a813be1e2dfca8efed
Keago99 <keatabde@gmail.com>
2024-01-17 15:12:02 +0200

    Skeleton for Todos planned out through comments and empty methods.

--- src/toDos.js
+++ src/toDos.js
@@ -1,9 +1,34 @@
+import { AllProjects } from "./projects";
+
 
 // A factory for Todo objects
 const toDoFactory = (title, description, dueDate, priority) => {
     return {title, description, dueDate, priority};
     
 };
+
+
+//This function will automate creating a div for each task.
+function createTaskDiv(){
+
+}
+
+
+
+// Creates a new task from the modal, this will actually be called in the modal class since it is tied to
+// that buttons logic, but because its primarily associated with the ToDo it will be housed here.
+function createTask(){
+
+}
+
+
+// A function that will loop through the Array and load all the tasks in the div using the "createTask"
+// function.
+function loadTasks(){
+
+}
+
+
 
 
 const basicTodo = toDoFactory("my first Todo!", "This is an important task", "15/04/2025", "High");
